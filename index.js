740677ff773ba89b19a792514bdf24873d1a9fa7
vishva-kalhara <vishvakalhara@gmail.com>
2024-01-15 17:25:29 +0530

    Updated CORS

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
+const { METHODS } = require("http");
 
 const io = require("socket.io")(server, {
-    cors: true,
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
 });
 
 app.use(cors());
@@ -13,7 +17,7 @@ const PORT = 5000;
 app.get("/", (req, res) => {
     res.status(200).json({
         status: "success",
-        message: "Response From Root",
+        message: "Response From Root UPdated",
     });
 });
 
